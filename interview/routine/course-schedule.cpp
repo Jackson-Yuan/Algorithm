24aa4563bb0aed9082d6890ce758df164ce63487
Jackson-Yuan <1436169116@qq.com>
2022-09-04 10:28:22 +0800

    For Interview

--- interview/routine/course-schedule.cpp
+++ interview/routine/course-schedule.cpp
@@ -5,22 +5,22 @@ using namespace std;
 
 
 /**
- * �����ѧ�ڱ���ѡ�� numCourses �ſγ̣���Ϊ0��numCourses - 1 ��
- * ��ѡ��ĳЩ�γ�֮ǰ��ҪһЩ���޿γ̡� ���޿γ̰�����prerequisites ����������prerequisites[i] = [ai, bi] ����ʾ���Ҫѧϰ�γ�ai �� ���� ��ѧϰ�γ� bi ��
- * ���磬���޿γ̶�[0, 1] ��ʾ����Ҫѧϰ�γ� 0 ������Ҫ����ɿγ� 1 ��
- * �����ж��Ƿ����������пγ̵�ѧϰ��������ԣ����� true �����򣬷��� false ��
- * ���ӣ�https://leetcode-cn.com/problems/course-schedule
+ * 你这个学期必须选修 numCourses 门课程，记为0到numCourses - 1 。
+ * 在选修某些课程之前需要一些先修课程。 先修课程按数组prerequisites 给出，其中prerequisites[i] = [ai, bi] ，表示如果要学习课程ai 则 必须 先学习课程 bi 。
+ * 例如，先修课程对[0, 1] 表示：想要学习课程 0 ，你需要先完成课程 1 。
+ * 请你判断是否可能完成所有课程的学习？如果可以，返回 true ；否则，返回 false 。
+ * 链接：https://leetcode-cn.com/problems/course-schedule
  * */
 
 /**
- * ����ֱ�ӳ�����ж�ͼ���Ƿ��л���bfs��dfs����
+ * 问题直接抽象成判断图中是否有环，bfs，dfs均可
  *
- * ע�����visited���飬��ֻ�Ǽ򵥵�bool������Ǽ򵥵�bool�����ǻᳬʱ
- * visited����������ѡ�0��1��2
- * 0������δ��������1���������У�2�����������
- * ֮���Զ����2����Ϊ��������dfsʱ���Ǵ�ÿ���㶼��һ��dfs����Ϊ�����ж��Ƿ��л����о��У�����ͳ�ƶ��ٸ�����
- * visitedΪ2��������������Ѿ��������ˣ����֮ǰ������ʱ���л����ٴ�����ѣ��ѳ�����������л��ظ������ˣ����û�����ٴν������
- * �������ѽ����Ȼû���������������ظ�����ģ���������ò���ֵ������������û�з��ʣ��ᵼ��ʱ�临�Ӷ�Ϊƽ�����Ӷ�
+ * 注意这边visited数组，不只是简单的bool，如果是简单的bool，怕是会超时
+ * visited在这有三个选项，0，1，2
+ * 0代表还未搜索到，1代表搜索中，2代表搜索完成
+ * 之所以多出个2是因为，我们在dfs时，是从每个点都做一遍dfs，因为我们判断是否有环（有就行，不是统计多少个环）
+ * visited为2代表从这个点我已经搜索过了，如果之前遍历的时候有环你再从这儿搜，搜出来结果还是有环重复搜索了；如果没环，再次进入这个
+ * 点往下搜结果依然没环，是用来避免重复计算的，否则如果用布尔值来仅仅区分有没有访问，会导致时间复杂度为平方复杂度
  * */
 class Solution {
 private:
--- test.cpp
+++ test.cpp
@@ -7,81 +7,32 @@
 using namespace std;
 
 
-class Solution {
-public:
-    string decodeString(string s) {
-        stack<int> sInt;
-        stack<string> sChar;
-
-        string ans;
-        for (int i = 0; i < s.length(); ++i) {
-            if (s[i] >= '0' && s[i] <= '9') {
-                string tmp;
-
-                int end = i + 1;
-                while (end < s.length() && s[end] != '[') ++end;
-                sInt.push(atoi(s.substr(i, end - i).c_str()));
-                int start;
-                if (s[end + 1] >= '0' && s[end + 1] <= '9') {
-                    sChar.push("");
-                    start = end + 1;
-                } else {
-                    sChar.push(string(1, s[end + 1]));
-                    start = end + 2;
-                }
-
-
-                while (!sChar.empty() && start < s.length()) {
-                    while (start < s.length() && s[start] != ']') {
-                        if (s[start] >= '0' && s[start] <= '9') {
-                            end = start + 1;
-                            while (end < s.length() && s[end] != '[') ++end;
-                            sInt.push(atoi(s.substr(start, end - start).c_str()));
-                            if (s[end + 1] >= '0' && s[end + 1] <= '9') {
-                                sChar.push("");
-                                start = end + 1;
-                            } else {
-                                sChar.push(string(1, s[end + 1]));
-                                start = end + 2;
-                            }
-
-                        } else {
-                            sChar.top().append(1, s[start]);
-                            ++start;
-                        }
-                    }
-
-
-                    int k = sInt.top();
-                    sInt.pop();
-                    string str = sChar.top();
-                    sChar.pop();
-
-                    string tmpStr;
-                    while (k--) {
-                        tmpStr += str;
-                    }
-
-                    tmp = tmpStr;
-
-                    if (!sChar.empty()) sChar.top().append(tmp);
-                    ++start;
-                }
-                i = start - 1;
-                ans.append(tmp);
-
-            } else {
-                ans.append(1, s[i]);
-            }
-        }
-
-
-        return ans;
-    }
-};
-
 int main() {
-    Solution solution;
+    int n, k;
+    cin >> n >> k;
 
-    cout << solution.decodeString("3[z]2[2[y]pq4[2[jk]e1[f]]]ef");
+    vector<char> str(n);
+    for (int i = 0; i < n; ++i) cin >> str[i];
+
+
+    vector<int> right(str.size(), 0);
+
+    if (str[str.size() - 1] == ')') right[str.size() - 1] = 1;
+
+    for (int i = str.size() - 2; i >= 0; --i) {
+        if (str[i] == ')') right[i] = right[i + 1] + 1;
+        else right[i] = right[i + 1];
+    }
+
+    int ans = 0;
+    for (int i = 0; i < str.size(); ++i) {
+        if (str[i] == '(') {
+            if (right[i] >= k) ans += right[i] - k + 1;
+        }
+    }
+
+    cout << ans << endl;
+    return 0;
+
+
 }
